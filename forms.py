a58000292c7377bd886897a1079034dd4b11c59d
Vishwa Muthuraman <vishwavm9@gmail.com>
2021-06-08 12:02:03 -0400

    whatever

--- forms.py
+++ forms.py
@@ -10,7 +10,7 @@ class VolunteerForm(FlaskForm):
     hours = DecimalField("Please enter the number of hours worked: ",validators=[DataRequired(), NumberRange(max=10)])
 
     event = SelectField("Select which event you attended",
-                 choices=[('WISE', 'WISE'), ('Food Drive', 'Food Drive'),('ARIS STEM Summit','ARIS STEM Summit'), ('Charity Miles','Charity Miles'),('Crocheting','Crocheting')])
+                 choices=[('WISE', 'WISE'),('WISTEM2D','WISTEM2D') ('Food Drive', 'Food Drive'),('ARIS STEM Summit','ARIS STEM Summit'), ('Charity Miles','Charity Miles'),('Crocheting','Crocheting'), ('Animal Shelter','Animal Shelter'), ('FLL BoroBlast', 'FLL BoroBlast'), ('IBM', 'IBM')])
 
     submit = SubmitField("Enter")
 
