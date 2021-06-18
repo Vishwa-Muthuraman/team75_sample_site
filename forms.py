88e3d90bbdf666f7268a5a92f811a342b16c2c31
Vishwa Muthuraman <vishwavm9@gmail.com>
2021-06-18 07:56:14 -0400

    whatever

--- forms.py
+++ forms.py
@@ -10,7 +10,7 @@ class VolunteerForm(FlaskForm):
     hours = DecimalField("Please enter the number of hours worked: ",validators=[DataRequired(), NumberRange(max=10)])
 
     event = SelectField("Select which event you attended",
-                 choices=[('WISE', 'WISE'), ('WISTEM2D', 'WISTEM2D'), ('Food Drive', 'Food Drive'),('ARIS STEM Summit','ARIS STEM Summit'), ('Charity Miles','Charity Miles'),('Crocheting','Crocheting'), ('Animal Shelter','Animal Shelter'), ('FLL BoroBlast', 'FLL BoroBlast'), ('IBM', 'IBM')])
+                 choices=[('WISE', 'WISE'), ('WISTEM2D', 'WISTEM2D'), ('Food Drive', 'Food Drive'),('ARIS STEM Summit','ARIS STEM Summit'), ('Charity Miles','Charity Miles'), ('Animal Shelter','Animal Shelter'),('Animal Shelter Prep Work(LEADS ONLY)','Animal Sheler Prep Work(LEADS ONLY)'), ('FLL BoroBlast', 'FLL BoroBlast'),('FLL BoroBlast Prep Work(LEADS ONLY)', 'FLL BoroBlast Prep Work(LEADS ONLY)'), ('IBM', 'IBM'), ('IBM Prep Work(LEADS ONLY)', 'IBM Prep Work(LEADS ONLY)')])
    
     Date = StringField("Type the date you are entering the form in the format (month date, year) Ex: June 15, 2021 ",validators=[DataRequired()])
 
