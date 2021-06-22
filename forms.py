c050ca9d909e83cd93c4e0008ee6ea1876355152
Vishwa Muthuraman <vishwavm9@gmail.com>
2021-06-22 16:27:42 -0400

    whatever

--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-from tkinter.constants import DISABLED
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, DecimalField, FloatField, DateField
 from wtforms.validators import DataRequired, NumberRange, Email, Length
@@ -13,7 +12,8 @@ class VolunteerForm(FlaskForm):
     event = SelectField("Select which event you attended",
                  choices=[('WISE', 'WISE'),('WISE Prep Work(LEADS ONLY)', 'WISE Prep Work(LEADS ONLY)') ,('WISTEM2D', 'WISTEM2D'), ('WISTEM2D Prep Work(LEADS ONLY)', 'WISTEM2D Prep Work(LEADS ONLY)'),('Food Drive', 'Food Drive'),('Food Drive Prep Work(LEADS ONLY)', 'Food Drive Prep Work(LEADS ONLY)'),('Charity Miles','Charity Miles'), ('Charity Miles Prep Work(LEADS ONLY)', 'Charity Miles Prep Work(LEADS ONLY)'),('Stem Kits', 'Stem Kits'),('Stem Kits Prep Work(LEADS ONLY)', 'Stem Kits Prep Work(LEADS ONLY)'),('Raider Pollinators', 'Raider Pollinators'),('Raider Pollinators Prep Work(LEADS ONLY)','Raider Pollinators Prep Work(LEADS ONLY)'), ('Safety', 'Safety'),('Safety Prep Work(LEADS ONLY)','Safety Prep Work(LEADS ONLY)'),('Animal Shelter','Animal Shelter'),('Animal Shelter Prep Work(LEADS ONLY)','Animal Sheler Prep Work(LEADS ONLY)'), ('FLL BoroBlast', 'FLL BoroBlast'),('FLL BoroBlast Prep Work(LEADS ONLY)', 'FLL BoroBlast Prep Work(LEADS ONLY)'), ('IBM', 'IBM'), ('IBM Prep Work(LEADS ONLY)', 'IBM Prep Work(LEADS ONLY)'), ('Quaransteam', 'Quaransteam'),('Quaransteam Prep Work(LEADS ONLY)','Quaransteam Prep Work(LEADS ONLY)'),('Precious Plastics', 'Precious Plastics'),('Precious Plastics Prep Work(LEADS ONLY)', 'Precious Plastics Prep Work(LEADS ONLY)'),('Outreach Tracking Website Prep Work(LEADS ONLY)','Outreach Tracking Website Prep Work(LEADS ONLY)'),('Team Website Prep Work(LEADS ONLY)','Team Website Prep Work(LEADS ONLY)')])
    
-
+    #Date = StringField("Enter the date your filling out this form in the format month-date-year(6-22-2021)", validators=[DataRequired(), Length(max =10)])
+    Date = DateField("Enter the date", default=date.today)
     
     submit = SubmitField("Enter")
 
